514e04c23bc449d1f2699798ee9b6084a778ef39
mricho <3901301+RiskoFPV@users.noreply.github.com>
2021-12-01 01:08:18 -0500

    Revert "edit mode is now on automatically"
    This reverts commit d48c63f432583820a24d6ca46f41f6225a2fa1b8.

--- src/libs/vis-custom-network/VisCustomNetwork.ts
+++ src/libs/vis-custom-network/VisCustomNetwork.ts
@@ -81,7 +81,6 @@ export default class VisCustomNetwork extends EventTarget {
         { from: from.id, to: id, eventual: to.id },
         { from: from.id, to: to.id, arrows },
       ]);
-      this.network.enableEditMode(); // enable edit mode on;
     });
   }
 
@@ -106,8 +105,6 @@ export default class VisCustomNetwork extends EventTarget {
         this.editNode();
       }      
     });
-
-    this.network.enableEditMode(); // enable edit mode on network that has just been loaded
   };
 
   addNode = (node: any, callback: any): void => {
