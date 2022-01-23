e96a9838443279334656f1d4366e5a8a873ae1c7
mricho <3901301+RiskoFPV@users.noreply.github.com>
2022-01-23 04:07:07 -0500

    set labelNode prop on created node

--- src/libs/vis-custom-network/VisCustomNetwork.ts
+++ src/libs/vis-custom-network/VisCustomNetwork.ts
@@ -77,6 +77,7 @@ export default class VisCustomNetwork extends EventTarget {
         level: 1,
       };
       this.nodes.add(labelNode);
+      node.labelNode = labelNode;
       this.nodes.add(node);
       //callback(node);
     });
