94d8f27c86d8210b601d7f76853861cd2cd07de9
mricho <3901301+RiskoFPV@users.noreply.github.com>
2022-02-11 16:24:41 -0500

    Graphs endpoint with full name/note/data search and save

--- graph/views.py
+++ graph/views.py
@@ -1,9 +1,30 @@
+from re import S
 from .serializers import GraphSerializer
 from .models import Graph
 from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.core import serializers
+from django.db.models import Q
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+@csrf_exempt
 def graphs(request):
-    graphs = Graph.objects.all()
-    serializer = GraphSerializer(graphs, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        search_query = "test3"
+        graphs = Graph.objects.filter(
+            Q(name__icontains=search_query) |
+            Q(data__icontains=search_query) |
+            Q(note__icontains=search_query)
+        )
+        serializer = GraphSerializer(graphs, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        json_data = json.loads(request.body)
+        try:
+            name = json_data['name']
+            note = json_data['note']
+            data = json_data['data']
+            Graph.objects.create(name=name, note=note, data=data)
+            return HttpResponse(status=201)
+        except KeyError:
+            return HttpResponse(status=400) 
