21b79046c0dc1e188f74f59a17a7addaa41fc144
mricho <3901301+RiskoFPV@users.noreply.github.com>
2022-03-13 16:33:44 -0400

    recenter undo/redo buttons

--- src/components/NetworkButtons.tsx
+++ src/components/NetworkButtons.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect } from "react";
-import { IconButton, Box } from "@mui/material";
+import { IconButton, Box, Stack } from "@mui/material";
 import { Undo, Redo, PanTool, Circle, ArrowRightAlt, Minimize, HighlightOff } from "@mui/icons-material";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
@@ -37,61 +37,63 @@ export default function NetworkButtons(props) {
     const classes = useStyles();
     return(
         <div>
-            <Box m={1}>
-                <IconButton 
-                    aria-label="Undo"
-                    disabled={props.undoDisabled}
-                    onClick={() => props.onUndo()}
-                >
-                    <Undo/>
-                </IconButton>
-                <IconButton 
-                    aria-label="Redo"
-                    disabled={props.redoDisabled}
-                    onClick={() => props.onRedo()}
-                >
-                    <Redo/>
-                </IconButton>
-            </Box>
-            <Box 
-              display="flex"
-              justifyContent="center"
-              alignItems="center">
-                <ToggleButtonGroup
-                    style={{'marginBottom': 10}}
-                    orientation="vertical"
-                    value={props.buttonMode} // The value of the selected button comes from App.tsx as a React prop
-                    exclusive
-                    onChange={handleChange}
-                    
+            <Stack direction="column" spacing={1}>
+                <Stack direction="row" spacing={1} justifyContent="center">
+                    <IconButton
+                        aria-label="Undo"
+                        disabled={props.undoDisabled}
+                        onClick={() => props.onUndo()}
                     >
-                    <ToggleButton value="pan" aria-label="pan" classes={{ selected: classes.selected }}>
-                        <IconButton aria-label="Pan">
-                            <PanTool/>
-                        </IconButton>
-                    </ToggleButton>
-                    <ToggleButton value="node" aria-label="node" classes={{ selected: classes.selected }}>
-                        <IconButton aria-label="Node">
-                            <Circle/>
-                        </IconButton>
-                    </ToggleButton>
-                    <ToggleButton value="directed-edge" aria-label="directed-edge" classes={{ selected: classes.selected }}>
-                        <IconButton aria-label="Directed Edge">
-                            <ArrowRightAlt style={{'transform': 'rotate(-45deg)'}}/>
-                        </IconButton>
-                    </ToggleButton>
-                    <ToggleButton value="edge" aria-label="edge" classes={{ selected: classes.selected }}>
-                        <IconButton aria-label="Undirected Edge">
-                            <Minimize style={{'transform': 'translate(-7px, -5px) rotate(-45deg)'}}/>
-                        </IconButton>
-                    </ToggleButton>
-                    <ToggleButton value="delete" aria-label="delete" classes={{ selected: classes.selected }}>
-                        <IconButton aria-label="Delete">
-                            <HighlightOff color="error"/>
-                        </IconButton>
-                    </ToggleButton>
-                </ToggleButtonGroup>
-            </Box>
-      </div>
+                        <Undo />
+                    </IconButton>
+                    <IconButton
+                        aria-label="Redo"
+                        disabled={props.redoDisabled}
+                        onClick={() => props.onRedo()}
+                    >
+                        <Redo />
+                    </IconButton>
+                </Stack>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center">
+                    <ToggleButtonGroup
+                        style={{ 'marginBottom': 10 }}
+                        orientation="vertical"
+                        value={props.buttonMode} // The value of the selected button comes from App.tsx as a React prop
+                        exclusive
+                        onChange={handleChange}
+
+                    >
+                        <ToggleButton value="pan" aria-label="pan" classes={{ selected: classes.selected }}>
+                            <IconButton aria-label="Pan">
+                                <PanTool />
+                            </IconButton>
+                        </ToggleButton>
+                        <ToggleButton value="node" aria-label="node" classes={{ selected: classes.selected }}>
+                            <IconButton aria-label="Node">
+                                <Circle />
+                            </IconButton>
+                        </ToggleButton>
+                        <ToggleButton value="directed-edge" aria-label="directed-edge" classes={{ selected: classes.selected }}>
+                            <IconButton aria-label="Directed Edge">
+                                <ArrowRightAlt style={{ 'transform': 'rotate(-45deg)' }} />
+                            </IconButton>
+                        </ToggleButton>
+                        <ToggleButton value="edge" aria-label="edge" classes={{ selected: classes.selected }}>
+                            <IconButton aria-label="Undirected Edge">
+                                <Minimize style={{ 'transform': 'translate(-7px, -5px) rotate(-45deg)' }} />
+                            </IconButton>
+                        </ToggleButton>
+                        <ToggleButton value="delete" aria-label="delete" classes={{ selected: classes.selected }}>
+                            <IconButton aria-label="Delete">
+                                <HighlightOff color="error" />
+                            </IconButton>
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </Box>
+            </Stack>
+        </div>
     )
 }
