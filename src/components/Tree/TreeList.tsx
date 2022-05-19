959470a42ef7b715223ad8167bfee60d652ec727
mricho <3901301+RiskoFPV@users.noreply.github.com>
2022-05-18 21:14:58 -0400

    Textbox with autocomplete below chips area, uses list of graphs as options

--- src/components/Tree/TreeList.tsx
+++ src/components/Tree/TreeList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import {
   Paper,
   TextField,
@@ -6,8 +6,15 @@ import {
   Stack,
   Typography,
   Divider,
-  Box
+  Box,
 } from "@mui/material";
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import { Tree, Graph } from "models";
 import TreeItem from "./TreeItem";
 
@@ -19,17 +26,18 @@ type IGraphListProps = {
     graphs: Graph[];
 };
 
+const filter = createFilterOptions<Graph>();
+
 const TreeList: React.FC<IGraphListProps> = (props) => {
   const trees = props.Trees;
-  const onClick = (node) => {
-    // console.log('Clicked', node);
-  }
-  const onHover = (node) => {
-    // console.log('Hover', node);
-  }
-  const onLeave = (node) => {
-    // console.log('Left', node);
-  }
+  const graphs = props.graphs;
+  const [value, setValue] = React.useState<Graph | null>(null);
+  
+  useEffect (() => {
+    if (value !== null) {
+      console.log('new value', value);
+    }
+  }, [value]);
   return (
     <Box sx={{ p: 2, border: '1px solid grey', borderRadius: '16px' }}>
       <Stack
@@ -42,8 +50,62 @@ const TreeList: React.FC<IGraphListProps> = (props) => {
               <TreeItem tree={tree} graphs={props.graphs} hoveredNodes={props.hoveredNodes} selectedNodes={props.selectedNodes} setHoveredChipToVis={props.setHoveredChipToVis}/>
             </Stack>
           )
-
         })}
+        <Autocomplete
+          value={value}
+          // onChange={(event, newValue) => {
+          //   if (typeof newValue === 'string') {
+          //     setValue({
+          //       name: newValue,
+          //     });
+          //   } else if (newValue && newValue.inputValue) {
+          //     // Create a new value from the user input
+          //     setValue({
+          //       name: newValue.inputValue,
+          //     });
+          //   } else {
+          //     setValue(newValue);
+          //   }
+          // }}
+          filterOptions={(options, params) => {
+            const filtered = filter(options, params);
+            console.log('filtered', filtered);
+            const { inputValue } = params;
+            // Suggest the creation of a new value
+            const isExisting = options.some((option) => inputValue === option.name);
+            if (inputValue !== '' && !isExisting) {
+              filtered.push({
+                inputValue,
+                name: `Add "${inputValue}"`,
+              });
+            }
+            return filtered;
+          }}
+          selectOnFocus
+          clearOnBlur
+          handleHomeEndKeys
+          id="free-solo-with-text-demo"
+          options={graphs}
+          getOptionLabel={(option) => {
+            // Value selected with enter, right from the input
+            if (typeof option === 'string') {
+              return option;
+            }
+            // Add "xxx" option created dynamically
+            if (option.inputValue) {
+              return option.inputValue;
+            }
+            // Regular option
+            return option.name;
+          }}
+          renderOption={(props, option) => <li {...props} key={option.id}>{option.name}</li>}
+          sx={{ width: 300 }}
+          freeSolo
+          renderInput={(params) => (
+            <TextField {...params} label="Type here..." />
+          )}
+        />
+        
       </Stack>
     </Box>
   );
