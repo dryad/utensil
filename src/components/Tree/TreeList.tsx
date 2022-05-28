7fcee120471d938a43447dbc338da87aaff5f6c2
mricho <3901301+RiskoFPV@users.noreply.github.com>
2022-05-28 10:23:11 -0400

    Add button

--- src/components/Tree/TreeList.tsx
+++ src/components/Tree/TreeList.tsx
@@ -14,6 +14,8 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import IconButton from '@mui/material/IconButton';
 
 import { Tree, Graph } from "models";
 import TreeItem from "./TreeItem";
@@ -36,21 +38,37 @@ const TreeList: React.FC<IGraphListProps> = (props) => {
 
   const [autoCompleteOpen, setAutoCompleteOpen] = React.useState(false);
   
-  useEffect(() => {
-    if (trees.length == 0) {
-      setAutoCompleteOpen(true);
-    }
-  }, [trees]);
+  // useEffect(() => {
+  //   if (trees.length == 0) {
+  //     setAutoCompleteOpen(true);
+  //   }
+  // }, [trees]);
 
   return (
-    <Box 
+    <Box
       sx={{ p: 2, border: '1px solid grey', borderRadius: '16px' }}
-      onClick={(event) => { 
-        if (event.target === event.currentTarget) {
-          setAutoCompleteOpen(true);
-        }
-      }}
+    // onClick={(event) => { 
+    //   if (event.target === event.currentTarget) {
+    //     setAutoCompleteOpen(true);
+    //   }
+    // }}
     >
+        {!autoCompleteOpen && (
+          <IconButton color="primary" aria-label="add" component="span">
+            <AddCircleOutlineIcon
+              onClick={() => {
+                setAutoCompleteOpen(true);
+              }}
+            style={{
+            //   position: "relative",
+              top: "0px",
+              right: "0px",
+            //   color: "rgba(0, 0, 0, 0.4)",
+            //   cursor: "pointer",
+            }}
+            />
+          </IconButton>
+        )}
       <Stack
         direction="column"
         spacing={1}
@@ -58,15 +76,14 @@ const TreeList: React.FC<IGraphListProps> = (props) => {
         {trees.map((tree, index) => {
           return (
             <Stack key={index} direction="row" spacing={1} justifyContent="center">
-              <TreeItem tree={tree} graphs={props.graphs} hoveredNodes={props.hoveredNodes} selectedNodes={props.selectedNodes} setHoveredChipToVis={props.setHoveredChipToVis}/>
+              <TreeItem tree={tree} graphs={props.graphs} hoveredNodes={props.hoveredNodes} selectedNodes={props.selectedNodes} setHoveredChipToVis={props.setHoveredChipToVis} />
             </Stack>
           )
         })}
-        { autoCompleteOpen && (   
-          <Stack key={'input'} direction="row" spacing={1} justifyContent="center">    
-            <TreeInput graphs={graphs} setAutoCompleteOpen={setAutoCompleteOpen} addNodeFromChips={props.addNodeFromChips} importGraphFromChips={props.importGraphFromChips}/>
+        {autoCompleteOpen && (
+          <Stack key={'input'} direction="row" spacing={1} justifyContent="center">
+            <TreeInput graphs={graphs} setAutoCompleteOpen={setAutoCompleteOpen} addNodeFromChips={props.addNodeFromChips} importGraphFromChips={props.importGraphFromChips} />
           </Stack>
-          
         )}
       </Stack>
     </Box>
