e83c82942a704cd0840b7fb69535c5471554cbf8
Aliona-D <alona.danyliuk@inventorsoft.co>
2023-10-17 17:44:17 +0300

    Add functions for comparing values

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -7,6 +7,11 @@ import { dateIsExpired } from './dateIsExpired';
 import { dateIsValid } from './dateIsValid';
 import { logarithm }  from './logarithm';
 import { naturalLogarithm } from './naturalLogarithm';
+import { isLessThan } from './isLessThan';
+import { isGreaterThan } from './isGreaterThan';
+import { isEqualTo } from './isEqualTo';
+import { isLessThanOrEqualTo } from './isLessThanOrEqualTo'; 
+import { isGreaterThanOrEqualTo } from './isGreaterThanOrEqualTo';
 
 export {
     addition,
@@ -17,5 +22,10 @@ export {
     dateIsExpired,
     dateIsValid,
     logarithm,
-    naturalLogarithm
+    naturalLogarithm,
+    isLessThan,
+    isGreaterThan,
+    isEqualTo,
+    isLessThanOrEqualTo,
+    isGreaterThanOrEqualTo
 };
